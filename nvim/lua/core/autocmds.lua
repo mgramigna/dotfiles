8143391b32551b48df54f0204d6cb64e19744498
mgramigna <mgramigna@mitre.org>
2022-06-30 09:36:29 -0400

    Add term autocmd for hiding numbers

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,1 +1,7 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, { pattern = { "*.md" }, command = "setlocal spell" })
+vim.api.nvim_create_autocmd("TermOpen", {
+	callback = function()
+		vim.opt_local.number = false
+		vim.opt_local.relativenumber = false
+	end,
+})
