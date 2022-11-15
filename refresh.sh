ff711bb1d58db5db3de2d010f79bd5148939217a
mgramigna <mgramigna@mitre.org>
2022-11-15 15:41:57 -0500

    Add alacritty dotfiles

--- refresh.sh
+++ refresh.sh
@@ -4,13 +4,16 @@ echo 'Copying files from home directory'
 
 rm -rf ./nvim
 rm -rf ./tmux
+rm -rf ./alacritty
 mkdir nvim
 mkdir tmux
+mkdir alacritty
 
 cp ~/.config/nvim/init.lua ./nvim
 cp -r ~/.config/nvim/lua ./nvim
 cp -r ~/.config/nvim/ftdetect ./nvim/
 cp -r ~/.config/nvim/after ./nvim/after
 cp -r ~/.config/tmux/tmux.conf ./tmux
+cp -r ~/.config/alacritty/ ./alacritty
 
 echo 'Done'
--- replace.sh
+++ replace.sh
@@ -5,8 +5,10 @@ timestamp=$(date +%s)
 
 mv ~/.config/nvim/ ~/.config/nvim-$timestamp.bak
 mv ~/.config/tmux/ ~/.config/tmux/tmux-$timestamp.bak
+mv ~/.config/alacritty/ ~/.config/alacritty-$timestamp.bak
 
 cp -r nvim ~/.config/
 cp -r tmux ~/.config/
+cp -r alacritty ~/.config/
 
 echo 'Done'
