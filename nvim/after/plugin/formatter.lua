31ea733e4c99f51a217fa31d6b22c3cf263a4302
mgramigna <mgramigna@mitre.org>
2022-12-22 11:00:43 -0500

    Add clang format

--- nvim/after/plugin/formatter.lua
+++ nvim/after/plugin/formatter.lua
@@ -1,6 +1,9 @@
 local util = require("formatter.util")
 require("formatter").setup({
 	filetype = {
+		c = {
+			require("formatter.filetypes.c").clangformat,
+		},
 		lua = {
 			require("formatter.filetypes.lua").stylua,
 		},
