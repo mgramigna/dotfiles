f41ea6b507c0b63770c68a57fa3a3b5ef1daac68
mgramigna <mgramigna@mitre.org>
2023-06-23 10:06:13 -0400

    Clip media text at 64 chars

--- sketchybar/plugins/media.sh
+++ sketchybar/plugins/media.sh
@@ -3,6 +3,11 @@ STATE="$(echo "$INFO" | jq -r '.state')"
 
 if [ "$STATE" = "playing" ]; then
   MEDIA="$(echo "$INFO" | jq -r '.app + ": " + .title + " - " + .artist')"
+
+  if [[ ${#MEDIA} -gt 64 ]]; then
+    MEDIA="${MEDIA:0:64}..."
+  fi
+
   sketchybar --set $NAME label="$MEDIA" drawing=on
 else
   sketchybar --set $NAME drawing=off
