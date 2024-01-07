f696aef01689e04fd939e75ab81d282831f97dca
Matt Gramigna <matthewgramigna@gmail.com>
2024-01-07 11:05:44 -0500

    chore: remove cmp-cmdline

--- nvim/lua/mg/plugins/cmp.lua
+++ nvim/lua/mg/plugins/cmp.lua
@@ -5,7 +5,6 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-cmdline",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
 		"onsails/lspkind.nvim",
@@ -78,22 +77,6 @@ return {
 					maxwidth = 50,
 				}),
 			},
-		})
-
-		cmp.setup.cmdline({ "/", "?" }, {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
-		})
-
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{ name = "cmdline" },
-			}),
 		})
 
 		require("luasnip.loaders.from_vscode").lazy_load({
