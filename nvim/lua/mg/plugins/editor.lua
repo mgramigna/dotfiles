86d287f1c32a40e31c69e4d2af3ad7f0e391f07e
Matt Gramigna <matthewgramigna@gmail.com>
2024-01-07 11:08:45 -0500

    feat: add autopairs

--- nvim/lua/mg/plugins/editor.lua
+++ nvim/lua/mg/plugins/editor.lua
@@ -1,5 +1,19 @@
 return {
 	{
+		"echasnovski/mini.pairs",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>up",
+				function()
+					vim.g.minipairs_disable = not vim.g.minipairs_disable
+				end,
+				desc = "Toggle auto pairs",
+			},
+		},
+	},
+	{
 		"tpope/vim-commentary",
 		event = "BufReadPre",
 	},
