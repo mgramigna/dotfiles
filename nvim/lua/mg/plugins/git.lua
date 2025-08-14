0c08e14f6f11669bb7b6c2125d6a29a84e43ffdd
mgramigna <matthewgramigna@gmail.com>
2025-08-13 21:18:02 -0400

    sync

--- nvim/lua/mg/plugins/git.lua
+++ nvim/lua/mg/plugins/git.lua
@@ -32,4 +32,19 @@ return {
 		"ruifm/gitlinker.nvim",
 		opts = {},
 	},
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 }
