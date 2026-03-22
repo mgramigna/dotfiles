702dbb02c83829062a134af6ef1a7c5196e74636
mgramigna <matthewgramigna@gmail.com>
2026-03-22 07:58:34 -0400

    sync

--- nvim/lua/mg/plugins/git.lua
+++ nvim/lua/mg/plugins/git.lua
@@ -15,19 +15,4 @@ return {
 		"ruifm/gitlinker.nvim",
 		opts = {},
 	},
-	{
-		"kdheepak/lazygit.nvim",
-		lazy = true,
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		dependencies = { "nvim-lua/plenary.nvim" },
-		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-	},
 }
--- nvim/lua/mg/plugins/snacks.lua
+++ nvim/lua/mg/plugins/snacks.lua
@@ -1,7 +1,20 @@
 return {
 	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
 	---@type snacks.Config
 	opts = {
 		input = {},
+		lazygit = {},
+	},
+	keys = {
+		-- Top Pickers & Explorer
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Toggle lazygit",
+		},
 	},
 }
